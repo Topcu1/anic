617fefd95cda4c6c8a718deb8e556d2920396287
Emre <topcu@live.de>
2018-06-06 17:38:12 +0200

    Product anzahl float;Products add wert

--- anic/üben/test/Product.java
+++ anic/üben/test/Product.java
@@ -2,5 +2,5 @@
 
 public class Product
 {
-	private int anzahl;
+	private float anzahl;
 }
--- anic/üben/test/Products.java
+++ anic/üben/test/Products.java
@@ -4,4 +4,5 @@ public class Product
 {
 	private TreeSet products;
 	private String name;
+	private int wert;
 }
